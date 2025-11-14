25ab55da8e9c49d8c2d9241dbfd29bd24d4001f2
Shubham Kumar Gupta <shubhamca76@gmail.com>
2025-11-14 00:57:43 -0600

    completed first intro section on rust programming

---
+++ hello_world/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // single line comment
+    /*
+    Multi
+    Line
+    Comment
+     */
+    println!("Hello World! I am a Rust Developer."); // this is not the standard way of writing comment !
+}
